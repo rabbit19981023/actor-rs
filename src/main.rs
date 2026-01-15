f650489071772966a04c2554c46569cee17075b1
Yu Chen <rabbit19981023@gmail.com>
2026-01-15 19:22:30 +0800

    refactor: implement simple actor with better design

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,7 @@ async fn main() {
     println!("count: {}", counter.get().await);
 }
 
-struct Counter {
-    sender: mpsc::Sender<CounterMsg>,
-}
+struct Counter;
 
 enum CounterMsg {
     Get { reply_to: oneshot::Sender<u32> },
@@ -22,46 +20,71 @@ enum CounterMsg {
     Decr(u32),
 }
 
-impl Counter {
-    fn spawn(init_state: u32, buffer: usize) -> Self {
+impl From<oneshot::Sender<u32>> for CounterMsg {
+    fn from(sender: oneshot::Sender<u32>) -> Self {
+        CounterMsg::Get { reply_to: sender }
+    }
+}
+
+impl Actor for Counter {
+    type State = u32;
+    type Msg = CounterMsg;
+
+    fn handle(state: &mut u32, msg: CounterMsg) {
+        match msg {
+            CounterMsg::Get { reply_to } => reply_to.send(*state).unwrap(),
+            CounterMsg::Incr(num) => *state += num,
+            CounterMsg::Decr(num) => *state -= num,
+        }
+    }
+}
+
+impl ActorHandle<Counter> {
+    pub async fn incr(&self, num: u32) {
+        self.send(CounterMsg::Incr(num)).await;
+    }
+
+    pub async fn decr(&self, num: u32) {
+        self.send(CounterMsg::Decr(num)).await;
+    }
+}
+
+trait Actor: Sized + 'static {
+    type State: Send;
+    type Msg: Send;
+
+    fn handle(state: &mut Self::State, msg: Self::Msg);
+
+    fn spawn(init_state: Self::State, buffer: usize) -> ActorHandle<Self> {
         let (sender, mut receiver) = mpsc::channel(buffer);
 
         tokio::spawn(async move {
             let mut state = init_state;
 
             while let Some(msg) = receiver.recv().await {
-                match msg {
-                    CounterMsg::Get { reply_to } => {
-                        reply_to.send(state).unwrap();
-                    }
-                    CounterMsg::Incr(num) => {
-                        state += num;
-                    }
-                    CounterMsg::Decr(num) => {
-                        state -= num;
-                    }
-                }
+                Self::handle(&mut state, msg)
             }
         });
 
-        Self { sender }
+        ActorHandle { sender }
     }
+}
 
-    async fn send(&self, msg: CounterMsg) {
+struct ActorHandle<A: Actor> {
+    sender: mpsc::Sender<A::Msg>,
+}
+
+impl<A: Actor> ActorHandle<A> {
+    async fn send(&self, msg: A::Msg) {
         self.sender.send(msg).await.unwrap()
     }
 
-    async fn get(&self) -> u32 {
+    async fn get(&self) -> A::State
+    where
+        A::Msg: From<oneshot::Sender<A::State>>,
+    {
         let (sender, receiver) = oneshot::channel();
-        self.send(CounterMsg::Get { reply_to: sender }).await;
+        self.send(A::Msg::from(sender)).await;
         receiver.await.unwrap()
-    }
-
-    async fn incr(&self, num: u32) {
-        self.send(CounterMsg::Incr(num)).await
-    }
-
-    async fn decr(&self, num: u32) {
-        self.send(CounterMsg::Decr(num)).await
     }
 }
